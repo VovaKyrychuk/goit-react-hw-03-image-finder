21852ce811a6a21584a142327b4c8f2668afa9f4
VovaKyrychuk <kyrychuk81@gmail.com>
2023-05-03 22:44:53 +0300

    add API, bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
-
 import { Searchbar } from './Searchbar/Searchbar';
 import { Layout } from './Layout/Layout';
 
-export class App extends Component {
+// export class App extends Component {
+//   state = {
+//     textSearch: '',
+//   };
+
+//   handleSubmit = textSearch => {
+//     this.setState({ textSearch });
+//   };
+//   render() {
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.handleSubmit} />
+//         <Layout></Layout>
+
+//         <GlobalStyle />
+//       </>
+//     );
+//   }
+// }
+
+import ImageGallery from './ImageGallery/ImageGallery';
+
+export default class App extends Component {
   state = {
     textSearch: '',
   };
 
+  // custom methods
   handleSubmit = textSearch => {
     this.setState({ textSearch });
   };
+
   render() {
+    const { textSearch } = this.state;
+
     return (
       <>
         <Searchbar onSubmit={this.handleSubmit} />
-        <Layout></Layout>
+        <Layout>
+          <ImageGallery value={textSearch} />
+        </Layout>
 
         <GlobalStyle />
       </>
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,121 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { getImages } from 'services/getApi';
+import { List, Text } from './ImageGallery.styled';
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
+import { Loader } from '../Loader/Loader';
+import { Button } from 'components/Button/Button';
+import Modal from 'components/Modal/Modal';
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+export default class ImageGallery extends Component {
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+  };
+
+  state = {
+    value: '',
+    images: [],
+    error: null,
+    status: Status.IDLE,
+
+    page: 1,
+    totalPages: 0,
+
+    isShowModal: false,
+    modalData: { tags: '' },
+  };
+
+  // перевіряємо, щоб в пропсах змінився запит
+  // y static відсутній this, тому дублюємо в state - search: ''
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.value !== nextProps.value) {
+      return { page: 1, value: nextProps.value };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page } = this.state;
+    const prevValue = prevProps.value;
+    const nextValue = this.props.value;
+    //   страхуємо від повторного запиту, якщо вже таке слово було введене
+    if (prevValue !== nextValue || prevState.page !== page) {
+      this.setState({ status: Status.PENDING });
+
+      // перевіряємо чи є помилка, якщо є - записуємо null
+      if (this.state.error) {
+        this.setState({ error: null });
+      }
+      getImages
+        .getImages(nextValue, page)
+        .then(images => {
+          this.setState(prevState => ({
+            images:
+              page === 1 ? images.hits : [...prevState.images, ...images.hits],
+            status: Status.RESOLVED,
+            totalPages: Math.floor(images.totalHits / 12),
+          }));
+        })
+        .catch(error => this.setState({ error, status: Status.REJECTED }));
+    }
+  }
+
+  // custom method to btn load
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  setModalData = modalData => {
+    this.setState({ modalData, isShowModal: true });
+  };
+
+  handleModalClose = () => {
+    this.setState({ isShowModal: false });
+  };
+
+  render() {
+    const { images, status, page, totalPages, isShowModal, modalData } =
+      this.state;
+
+    if (status === 'pending') {
+      return <Loader />;
+    }
+
+    if (images.length === 0) {
+      return (
+        <Text
+          message={`Oops... there are no images matching your search... `}
+        />
+      );
+    }
+
+    if (status === 'resolved') {
+      return (
+        <>
+          <List>
+            {images.map(image => (
+              <ImageGalleryItem
+                key={image.id}
+                item={image}
+                onImageClick={this.setModalData}
+              />
+            ))}
+          </List>
+          {images.length > 0 && status !== 'pending' && page <= totalPages && (
+            <Button onClick={this.handleLoadMore}>Load More</Button>
+          )}
+          {isShowModal && (
+            <Modal modalData={modalData} onModalClose={this.handleModalClose} />
+          )}
+        </>
+      );
+    }
+  }
+}
